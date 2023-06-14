c5371806e956a5430c86abb32dd5188f4488c96e
Shiyuan Zhang <shiyuanzhang33@gmail.com>
2023-06-14 13:52:25 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,3 @@
-import React, { useState } from 'react';
-import './App.css';
-<<<<<<< HEAD
-import { Button, Container, Row, Col } from 'reactstrap';
-import TaskPage from './pages/TaskPage';
-import { TASKS } from './app/shared/TASKS';
-
-function App() {
-  //return function showing components on the page
-  //Todo - Update Task
-  const [taskList, setTaskList] = useState(TASKS);
-
-  const addTask = () => {
-    //TODO - add formik
-    const newTask = {
-      id: taskList.length,
-      title: "Phase 1 user testing",
-      status: "Done",
-      description: "Some quick example text to build on the card title and make up the bulk of the card‘s content."
-    }
-
-    setTaskList([...taskList, newTask]);
-
-} 
-
-  return (
-    <Container>
-      <Row>
-        <Col>
-        <TaskPage/>
-        </Col>
-        <Col className='mt-10px'>
-        <Button onClick={()=> addTask()} className='btn-sm bg-primary'>Add new Task</Button>
-        </Col>
-      </Row>
-    </Container>
-  );
-}
-=======
 import { Button, Card, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
 import TasksList from './features/components/TasksList';
 import { TASKS } from './features/components/TASKS';
@@ -73,7 +34,5 @@ function App() {
     </>
   )
 };
-
->>>>>>> temp
 
 export default App;
